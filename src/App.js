4c6c9cd884b81fb2f35857c061bba7046eed431f
elerinnoorkoiv <elerin12@gmail.com>
2024-01-26 12:22:15 +0200

    ExpenseDate css

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import ExpenseItem from './components/ExpenseItem'
 function App() {
   const expenses = [
     {
-      date: new Date(2023, 9, 6),
+      date: new Date(2024, 0, 26),
       title: 'New book',
       price: 30.99 
     },
     {
-      date: new Date(2023, 9, 6),
+      date: new Date(2024, 0, 26),
       title: 'New jeans',
       price: 99.99 
     },
