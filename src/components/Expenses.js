01879fd7212cc9fb36129700b67e71cfcdb8ef04
elerinnoorkoiv <elerin12@gmail.com>
2024-01-26 13:38:06 +0200

    expenses

---
+++ src/components/Expenses.js
@@ -0,0 +1,25 @@
+import './Expenses.css'
+import ExpenseItem from "./ExpenseItem"
+
+function Expenses(props){
+    const expenses = [
+        {
+          date: new Date(2024, 0, 26),
+          title: 'New book',
+          price: 30.99 
+        },
+        {
+          date: new Date(2024, 0, 26),
+          title: 'New jeans',
+          price: 99.99 
+        },
+      ]
+    return(
+        <div className="expenses">
+            <ExpenseItem expenseData={expenses[0]} />
+            <ExpenseItem expenseData={expenses[1]} />
+        </div>
+    )
+}
+
+export default Expenses
