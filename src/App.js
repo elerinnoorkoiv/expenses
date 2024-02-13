1af71e0cf8634aacef9c1a2a966e05a22c74c8d3
elerinnoorkoiv <elerin12@gmail.com>
2024-02-13 15:07:25 +0200

    andmete kuvamine nimekirjast

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
+import { useState } from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DYMMY_EXPENSES =[
+  {
+    id: 'id1',
+    date: new Date(2024, 0, 19),
+    title: 'New Book',
+    price: 39.99
+  },
+  {
+    id: 'id2',
+    date: new Date(2024, 0, 19),
+    title: 'New Jeans',
+    price: 99.99
+  },
+]
+
 const App = () => {
-  const expenses =[
-    {
-      date: new Date(2024, 0, 19),
-      title: 'New Book',
-      price: 39.99
-    },
-    {
-      date: new Date(2024, 0, 19),
-      title: 'New Jeans',
-      price: 99.99
-    },
-  ]
+  const [expenses, setExpenses] = useState(DYMMY_EXPENSES)
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js')
-    console.log(expense)
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses]
+    })
   }
   
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses expenses={Expenses} />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,26 +4,22 @@ import ExpenseItem from './ExpenseItem'
 import ExpensesFilter from './ExpensesFilter'
 
 const Expenses = (props) => {
-    const expenses = [
-        {
-          date: new Date(2024, 0, 26),
-          title: 'New book',
-          price: 30.99 
-        },
-        {
-          date: new Date(2024, 0, 26),
-          title: 'New jeans',
-          price: 99.99 
-        },
-      ]
+  props.expenses.map((expense) => {
+    console.log(expense)
+  })
+    
     const saveSelectedYearHandler = (selectedYear) => {
       console.log(selectedYear)
     }
     return(
         <div className='expenses'>
             <ExpensesFilter saveSelectedYear = {saveSelectedYearHandler}/>
-            <ExpenseItem expenseData={expenses[0]} />
-            <ExpenseItem expenseData={expenses[1]} />
+            {
+              props.expenses.map((expense) => {
+                return <ExpenseItem expenseData={expense} key={expense.id}/>
+              })
+            }
+
         </div>
     );
 };
