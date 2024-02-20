b1e3e0f15c8aea0288cd054d4a056a0a32d2fcdb
elerinnoorkoiv <elerin12@gmail.com>
2024-02-20 13:21:36 +0200

    jsx and css

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,26 +2,29 @@ import './Expenses.css'
 import Card from '../UI/Card'
 import ExpenseItem from './ExpenseItem'
 import ExpensesFilter from './ExpensesFilter'
+import { useState } from 'react'
 
 const Expenses = (props) => {
-  props.expenses.map((expense) => {
-    console.log(expense)
-  })
-    
-    const saveSelectedYearHandler = (selectedYear) => {
-      console.log('Year data in Expenses.js', selectedYear)
-    }
-    return(
-        <div className='expenses'>
-            <ExpensesFilter saveSelectedYear = {saveSelectedYearHandler}/>
-            {
-              props.expenses.map((expense) => {
-                return <ExpenseItem expenseData={expense} key={expense.id}/>
-              })
-            }
+  const [filteredYear, setFilteredYear] = useState('2024')
 
-        </div>
-    );
-};
+  const saveSelectedYearHandler = (selectedYear) => {
+    console.log('Year data in Expenses.js', selectedYear)
+    setFilteredYear(selectedYear) // Lisa see rida
+  }
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
+  return (
+    <Card className='expenses'>
+      <ExpensesFilter saveSelectedYear={saveSelectedYearHandler}/>
+      {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+      {filteredExpenses.length > 0 && filteredExpenses.map((expense) => (
+        <ExpenseItem expenseData={expense} key={expense.id}/>
+      ))}
+    </Card>
+  )
+}
 
 export default Expenses
